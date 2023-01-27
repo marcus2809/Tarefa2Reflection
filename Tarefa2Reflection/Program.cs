558ad416f824bee8dcc4093cddd29cc5f172d7a5
Marcus Vinícius <marcusvinicius.paz2001@gmail.com>
2023-01-27 14:59:16 -0300

    Adicionar arquivos de projeto.

---
+++ Tarefa2Reflection/Program.cs
@@ -0,0 +1,50 @@
+﻿var reflection = new Reflection<Pessoa>();
+
+reflection.Detalhar(new Pessoa());
+var novaPessoa = reflection.Instanciar(new Pessoa());
+Console.WriteLine(novaPessoa.GetType());
+
+class Reflection<T>
+{
+    public void Detalhar(T obj)
+    {
+        var type = obj.GetType();
+
+        var props = type.GetProperties();
+        var methods = type.GetMethods();
+
+        Console.WriteLine("Propriedades");
+        foreach (var prop in props)
+        {
+            Console.WriteLine(prop);
+        }
+
+        Console.WriteLine("Métodos");
+        foreach (var method in methods)
+        {
+            Console.WriteLine(method);
+        }
+    }
+
+    public T Instanciar(T obj)
+    {
+        var novoObj = Activator.CreateInstance<T>();
+        return novoObj;
+    }
+}
+
+class Pessoa
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public Pessoa()
+    {
+
+    }
+
+    public bool ValidarPessoa()
+    {
+        return true;
+    }
+}
